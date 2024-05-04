ddf9156ff0daa0d83d5c9a2136f5206abf616e20
OKAINO <okertepe@gmail.com>
2024-05-04 12:14:55 +0300

    Second Commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <NavBar></NavBar>
+  <div class="w-max bg-no-repeat bg-cover bg-center" style="background-size: 100% auto;">
+    <img src="./assets/dentistAi.png" class="h-max w-max filter grayscale" alt="Flowbite Logo"></img>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <Footer></Footer>
+  </template>
+  
+<script>
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+  export default {
+    components: {
+      NavBar,
+      Footer
+    },
+  data() {
+    return {
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    }
+  },
+  methods: {
+
+  }
+  }
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,57 @@
+<template>
+    <footer class="footer">
+      <div class="footer-section">
+        <h4>About Us</h4>
+        <p>Learn more about our company, mission, and values.</p>
+      </div>
+      <div class="footer-section">
+        <h4>Contact</h4>
+        <p>Email us at info@example.com</p>
+      </div>
+      <div class="footer-section">
+        <h4>Follow Us</h4>
+        <p>Follow us on social media for updates.</p>
+      </div>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: "Footer",
+  }
+  </script>
+  
+  <style scoped>
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    background-color: rgb(3, 7, 18);
+    color: white;
+    padding: 20px;
+    text-align: center;
+  }
+  
+  .footer-section h4 {
+    margin: 0 0 10px 0;
+  }
+  
+  .footer-section p {
+    margin: 0;
+  }
+  
+  @media (max-width: 600px) {
+    .footer {
+      flex-direction: column;
+      align-items: center;
+    }
+  
+    .footer-section {
+      margin-bottom: 20px;
+    }
+  
+    .footer-section:last-child {
+      margin-bottom: 0;
+    }
+  }
+  </style>
+  
