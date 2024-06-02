4ba4e018ab8c01024d02387015215c1205b3f807
OKAINO <okertepe@gmail.com>
2024-06-02 17:31:49 +0300

    Third Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'dentist': 'url("./src/assets/yakın.JPG")',
+        'dentist-birdseye': 'url("./src/assets/uzakkus.JPG")',
+        'glass': 'url("./src/assets/glass.JPG")',
+        'shelf': 'url("./src/assets/shelf.JPG")'
+      },
+      fontFamily: {
+        julius: ['"Julis Sans One"'],
+      }
+    },
   },
   plugins: [],
 }
